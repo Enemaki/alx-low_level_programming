3c04162c0492ce43b62147618845e24c48f8c156
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-02 13:57:50 -0700

    5-task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- print digit numbers of base 10
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+
+	return (0);
+}
