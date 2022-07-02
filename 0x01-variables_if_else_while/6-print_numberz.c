6ba07c5703ea9d476c9937a211d69b63d2eacca2
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-02 14:10:48 -0700

    6th task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- print digit number in base 10
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar("\n");
+
+	return (0);
+}
