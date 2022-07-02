a03f78b0a89cee7884f83aad431b9ccc75fa5fd0
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-02 15:30:34 -0700

    7-task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- print lowercase alphabet in reverse
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char alph;
+
+	alph = 'z';
+
+	while (alph >= a)
+	{
+		putchar(alph);
+		alph--;
+	}
+	putchar('\n');
+
+	return (0);
+}
