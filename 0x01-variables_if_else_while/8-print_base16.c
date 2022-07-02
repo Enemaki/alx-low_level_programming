9d2db009de8cbd0a8919b19e93c60f1f3c4e68ed
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-02 15:48:51 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main- print numbers of base 16 in lowercase
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num;
+	int alph;
+
+	num = 0;
+	alph = 'a';
+
+	while(num <= 15)
+	{
+		if (num <= 9)
+		{
+			putchar(num);
+			num++;
+		}
+		else
+		{
+			putchar(alph);
+			alph++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
