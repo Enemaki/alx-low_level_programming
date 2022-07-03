22dc27faf00901db0cb7294198491387b82131fe
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-02 17:09:00 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main- print all combination of 2 digits
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			if ((i < j) && (j <= '9'))
+			{
+				putchar(i);
+				putchar(j);
+				if ((j < '9') | (i < '8'))
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
