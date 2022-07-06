6c241d900c640c97ac462a6c56989640f9822ae6
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-06 16:25:38 -0700

    11-task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,12 +16,13 @@ void print_to_98(int n)
 		{
 			if (i != 98)
 			{
-				printf("%d,", i);
+				printf("%d, ", i);
 			}
 			else if (i == 98)
 			{
 				printf("%d\n", i);
 			}
+		}
 	}
 	else if (n >= 98)
 	{
@@ -33,7 +34,7 @@ void print_to_98(int n)
 			}
 			else if (j == 98)
 			{
-				printf("%d\n", j);
+				printf("%d\n ", j);
 			}
 		}
 	}
