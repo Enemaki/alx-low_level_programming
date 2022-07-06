03ba5e4275d7f965ef5c0be92b9dfe6b23461a82
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-06 08:23:21 -0700

    2-task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- function prints alphabets x10
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char alph = 'a';
+
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+			alph++;
+		}
+		_putchar('\n');
+	}
+	i++;
+}
