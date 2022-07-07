6df8b70f5bb90889dce1ad04e454a21660e9cbe6
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-06 17:23:19 -0700

    100th

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)				
+			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
 				if (j == 0)
@@ -27,7 +27,6 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(k + '0');
 				}
-					
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
