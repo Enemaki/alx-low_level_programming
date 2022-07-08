2adea2ed928f95d2440eb28f7726acd7a13f4660
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-08 14:59:12 -0700

    6-task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line- function draws a straight line
+ * @n: is the argument which the function takes
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
