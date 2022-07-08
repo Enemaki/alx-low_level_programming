1a8302e2a819fa4f55b4e5a8479bf68b59a70072
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-08 16:16:25 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,27 +10,23 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%i", i);
-			printf(" ");
 		}
 		if (i != 100)
 		{
