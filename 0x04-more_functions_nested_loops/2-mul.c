696f7206c3b4867d339ecdd62e0d10036b014d19
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-08 09:29:21 -0700

    2-task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul- multiply two integers
+ * @a: int type number 1
+ * @b: int type number 2
+ * Return: return answer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
