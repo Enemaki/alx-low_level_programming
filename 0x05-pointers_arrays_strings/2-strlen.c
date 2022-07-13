a0a477e08bb8d73b8252ec923f97ee6ec8db4433
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-13 10:48:27 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen- function returns the length of a string
+ * @s: string parameter to be printed
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		_putchar(s[n]);
+		n++;
+	}
+}
