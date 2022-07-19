174e54ef0a363ab95cd828b0a728243729640868
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-19 06:03:38 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,27 +10,12 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				sum1 = sum1 + a[i][j];
-			}
-		}
-	}
-	for (i = size - 1; i >= 0; i--)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				sum2 = sum2 + a[i][j];
-			}
-		}
+		sum1 += a[(size + 1) * i];
+		sum2 += a[(size - 1) * (i + 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
