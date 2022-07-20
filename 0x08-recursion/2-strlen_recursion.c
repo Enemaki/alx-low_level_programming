74c228fcf52a9ae442ad365153c7679c46350f1f
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-20 15:52:24 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strlen_recursion- function that returns the length of a string
+ * @s: string to be used
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length, i = 0;
+
+	length = 0;
+
+	if (s[i] != '\0')
+	{
+		length += 1;
+		_strlen_recursion(&s[i + 1]);
+	}
+	else
+	{
+		return (0);
+	}
+}
