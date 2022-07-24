7924303f259b7f25ac5b07ba81ed8ffebde00973
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-24 10:21:25 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- program that prints its name, followed by a new line
+ * @argc: count of arguments
+ * @argv: array of arguments to main
+ * Return: program name
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			count++;
+		}
+	}
+	printf("%d", count);
+	return (0);
+}
