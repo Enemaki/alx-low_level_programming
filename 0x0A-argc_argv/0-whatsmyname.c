6b4f46e8a884021d64cdb5f3dd2d565531ed5ab3
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-24 09:44:34 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main- program that prints its name, followed by a new line
+ * @argc: count of arguments
+ * @argv: array of arguments to main
+ * Return: program name
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+}
