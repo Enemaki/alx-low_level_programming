08e49884b451f427e6e490dbffd3346fba33dcd7
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-25 17:01:41 -0700

    0-task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	int i;
 
 	if (size != 0)
 	{
 		str = malloc(sizeof(char) * size);
-		str[0] = c;
+		for (i = 0; i < size; i++)
+		{
+			str[i] = c;
+		}
 		return (str);
+		free(str);
 	}
 
 	return (NULL);
