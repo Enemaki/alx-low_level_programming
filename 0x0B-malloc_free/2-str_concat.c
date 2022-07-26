7f187b3e90bb9fff5aee2b0eb3d412646a8cccf7
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-26 05:33:30 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,37 +11,40 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *new_s;
-	int i, j, count1 = 1, count2 = 1;
+	int i, j, count1 = 0, count2 = 0;
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	while (s1[count1])
 	{
 		count1++;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	while (s2[count2])
 	{
 		count2++;
 	}
+	count2++;
 	new_s = malloc(sizeof(char) * (count1 + count2));
-	for (i = 0; i < count1; i++)
-	{
-		new_s[i] = s1[i];
-	}
-	for (j = 0; j < count2; j++)
-	{
-		new_s[i] = s2[j];
-		i++;
-	}
 	if (new_s == NULL)
 	{
 		return (NULL);
+	}
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		new_s[j++] = s1[i++];
+	}
+	i = 0;
+	while (s2[i])
+	{
+		new_s[j++] = s2[i++];
 	}
 	return (new_s);
 }
