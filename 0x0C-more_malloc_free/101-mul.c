d0b5748b8710b472f4ec02e0183954b2b4184e1b
Sadiku Mustapha <mustyeneye@gmail.com>
2022-07-29 15:28:30 -0700

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -49,7 +49,7 @@ char *create_xarray(int size)
 }
 /**
  * iterate_zeroes - Iterates through a string of numbers containing
- *                  leading zeroes until it hits a non-zero number.
+ * leading zeroes until it hits a non-zero number.
  * @str: The string of numbers to be iterate through.
  * Return: A pointer to the next non-zero element.
  */
@@ -86,7 +86,7 @@ int get_digit(char c)
  * @digit: The single digit.
  * @zeroes: The necessary number of leading zeroes.
  * Description: If mult contains a non-digit, the function
- *              exits with a status value of 98.
+ * exits with a status value of 98.
  */
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
@@ -171,7 +171,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
  * @argv: The number of arguments passed to the program.
  * @argc: An array of pointers to the arguments.
  * Description: If the number of arguments is incorrect or one number
- *              contains non-digits, the function exits with a status of 98.
+ * contains non-digits, the function exits with a status of 98.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
@@ -195,7 +195,7 @@ int main(int argc, char *argv[])
 	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
 	{
 		printf("0\n");
-		printf("0\n");
+		return (0);
 	}
 	size = find_len(argv[1]) + find_len(argv[2]);
 	final_prod = create_xarray(size + 1);
