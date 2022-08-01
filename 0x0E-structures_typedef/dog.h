51eab0df90afb4a1e205b843fbf2de2230fc642e
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-01 12:26:39 -0700

    task 0- struct decl

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include "main.h"
+
+/**
+ * struct dog- struct declaration
+ * @name: variable 1 type char
+ * @age: variable 2 type integer
+ * @owner: variable 3 type char
+ * Return: nothing
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
