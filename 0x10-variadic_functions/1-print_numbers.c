b381fae0cd7bfed9ee0945e4f892f6c79295cfad
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-05 00:57:21 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,24 +11,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-
-	if (separator == NULL)
-	{
-		return;
-	}
-	if (n == 0)
-	{
-		return;
-	}
 	
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i < n - 1)
+		if (separator != NULL && i < n - 1)
 		{
-			printf("%c", *separator);
+			printf("%s", *separator);
 		}
 	}
 	va_end(ap);
