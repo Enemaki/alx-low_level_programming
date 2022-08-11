39bc8551274efd683f39150813f5ad86a3f06abf
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-11 16:58:37 -0700

    free node

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list- function that frees a list_t list
+ * @head: pointer to node
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != null)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
