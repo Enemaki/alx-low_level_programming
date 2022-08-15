cf6235897520b39357d4d2002171daa726765f2b
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-15 15:23:00 -0700

    5

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,9 +9,11 @@ void free_listint(listint_t *head)
 {
 	listint_t *current;
 
-	while ((current = head) != NULL)
+	while (head->next != NULL)
 	{
-		free(current);
-		head = head->next;
+		current = head->next;
+		free(head);
+		head = current;
 	}
+	free(head);
 }
