4d398ab12afca9b867debd4ffe53da21b806d50b
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-16 09:47:11 -0700

    9th

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	if (idx == 0)
-		new = temp;
+		new->next = temp;
 	else
 	{
 		ptr = temp->next;
