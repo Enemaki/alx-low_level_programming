1c36667e1add56f2562312b9ab35b402b529a267
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-19 15:47:29 -0700

    replace bit with 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * clear_bit- function that sets the value
+ * of a bit to 1 at a given index
+ * @n: pointer to integer
+ * @index: index of bit to be replaced
+ * Return: bit or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int rep;
+
+	if (index > (sizeof(8) * 8))
+		return (-1);
+	rep = 0;
+	rep = rep << index;
+	*n = ((*n) & rep);
+	return (0);
+}
