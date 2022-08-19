26d04af6862dba39a4a99e0f931888154aa28437
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-19 15:58:05 -0700

    replace bit with 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,8 +12,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(8) * 8))
 		return (-1);
-	rep = 0;
+	rep = 1;
 	rep = rep << index;
-	*n = ((*n) & rep);
+	if (((*n >> index) & 1) == 1)
+		*n = rep ^ *n;
 	return (1);
 }
