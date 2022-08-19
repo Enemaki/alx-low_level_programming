16c4eceb8ba321bc59d1e9a6e65b9c3867a0d670
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-19 13:00:56 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- * myatoi- convert a string to an int
- * @s: string value
- * Return: integer value
- */
-unsigned int myatoi(const char *s)
-{
-	unsigned int res = 0;
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		res = res * 10 + s[i] - '0';
-	}
-	return (res);
-}
-/**
  * binary_to_uint- function that converts a binary number
  * to an unsigned int
  * @b: string variable
@@ -24,23 +8,30 @@ unsigned int myatoi(const char *s)
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int num, decimal_num = 0, base = 1, rem;
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len = 0;
+	unsigned int j = 0;
 
 	if (b == NULL)
+	{
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	}
+	while (b[j] != '\0')
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
+		len++;
+		j++;
+	}
+	for (c = len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
 			return (0);
+		if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
 	}
-	num = myatoi(b);
-	while (num > 0)
-	{
-		rem = num % 10;
-		decimal_num = decimal_num + rem * base;
-		num = num / 10;
-		base = base * 2;
-	}
-	return (decimal_num);
+	return (i);
 }
