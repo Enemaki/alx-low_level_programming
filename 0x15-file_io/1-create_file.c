d9a9748a14e149416c70dffe751d26bb6b5c25e7
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-23 06:38:00 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,8 +37,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		close(fd);
-		return (1);
+		text_content = "";
 	}
 	wr = write(fd, text_content, ld);
 	if (wr == -1)
