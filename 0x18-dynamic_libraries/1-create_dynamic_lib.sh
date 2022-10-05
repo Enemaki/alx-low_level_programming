d18ff790830b6866b369666498414516354d576b
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-10-05 07:08:33 -0700

    dynamic lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c -fpic *.c
-gcc -shared -o liball.so *.o
+gcc -c -fPIC *.c
+gcc -shared -o libdynamic.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
