fea36030542fd5c800444674f5819ddd2ec44856
Sadiku Mustapha Eneye <mustyeneye@gmail.com>
2022-10-05 04:29:08 -0700

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,8 +23,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	head->prev = newnode;
-	newnode->next = head;
-	head = newnode;
+	newnode->next = *head;
+	*head = newnode;
 
 	return (newnode);
 }
